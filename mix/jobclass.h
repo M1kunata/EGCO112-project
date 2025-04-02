64c912fbe3a0786ce8019b4d787accef427e00cd
6713221 จักริน <jakkarin.roe@student.mahidol.edu>
2025-04-02 10:14:54 +0000

    อ่านไฟล์job

--- mix/jobclass.h
+++ mix/jobclass.h
@@ -6,20 +6,36 @@ using namespace std;
 class job
 {
     private:
+        int tag_num=0;
         string jobtitle,company,decription,location,status;
         float salary;
     public:
-        void add_job(string head,string company,string detail,string loca,float money);
-        
+        void add_job(string head,string company,string detail,string location,float money,string status="recruiting");
+        void read_job();
+        void edit_job();
 };
-void job::add_job(string head,string company,string detail,string loca,float money)
+void job::add_job(string head,string company,string detail,string loca,float money,stirng stas)
 {
+    read_job();
+    tag_num++;
     ofstream F;
     F.open("job_data.txt",ios::app);//เปิดไฟล์โหมดเติมท้ายกับสร้าง
-    F<<head<<" "<<company<<" "<<detail<<" "<<money<<endl;
+    F<<tag_num<<" "<<head<<" "<<company<<" "<<detail<<" "<<loca<<" "<<money<<" "<<stas<<endl;
     F.close();
 }
-
+void job::read_job()
+{
+    ifstream Out("job_data.txt");
+    if(Out)
+    {
+    string line;
+    while(getline(Out,line))//อ่านค่าจากไฟล์
+    {
+        istringstream iss(line);
+        iss>>tag_num>>jobtitle>>company>>detail>>location>>money>>status;//แยกข้อมูลใส่ตัวแปร
+    }
+    }
+}
 
 
 
